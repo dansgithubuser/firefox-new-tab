b9b79723accdd0e481931a8ea13fc1b6efb5a8ad
Dan <dansonlinepresence@gmail.com>
2018-05-10 00:01:00 -0400

    disable ctrl-q and ctrl-shift-w

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.getElementById('key_quitApplication').remove();
+document.getElementById('key_closeWindow').remove();
 browser.commands.onCommand.addListener(function(command){
 	if(command=='new-tab-extension-command')
 		browser.storage.local.get().then((results)=>{
