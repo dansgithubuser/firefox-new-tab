428b5a66ba9cd17bbcf09be6e82e1c3e2c11d650
Dan <dansonlinepresence@gmail.com>
2018-05-10 00:23:54 -0400

    Revert "disable ctrl-q and ctrl-shift-w"
    This reverts commit b9b79723accdd0e481931a8ea13fc1b6efb5a8ad.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-document.getElementById('key_quitApplication').remove();
-document.getElementById('key_closeWindow').remove();
 browser.commands.onCommand.addListener(function(command){
 	if(command=='new-tab-extension-command')
 		browser.storage.local.get().then((results)=>{
